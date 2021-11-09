54234a796c161a62a3f499915b8cff5cc1f3ace3
Reinhardt <notmarx.tech@gmail.com>
2021-11-09 16:16:59 +0700

    Use custom log

--- src/api/database/index.ts
+++ src/api/database/index.ts
@@ -1,12 +1,13 @@
 import { Database as QuickMongoDatabase } from "quickmongo";
 import { MONGODB_URI } from "../../../config.json";
+import { Logger } from "../../utils";
 
 export class Database {
-    public db = new QuickMongoDatabase(MONGODB_URI);
+    public db = new QuickMongoDatabase(MONGODB_URI, "Belfast");
 
     public async launch() {
         this.db.on("ready", () => {
-            console.log("[DATABASE] - MongoDB Successfully Connected");
+            Logger.success("MONGODB", `MongoDB: ${this.db.name} Successfully Connected`);
         });
     }
 }
--- src/events/gatewayready.ts
+++ src/events/gatewayready.ts
@@ -1,9 +1,10 @@
 import { Event } from "../interfaces";
 import { ClientEvents } from "detritus-client/lib/constants";
+import { Logger } from "../utils";
 
 export const event: Event = {
     name: ClientEvents.GATEWAY_READY,
     run: async (client) => {
-        console.log(`${client.user.tag} Has Gone ${client.gateway.presence.status.toUpperCase()}`);
+        Logger.system("GATEWAY", `${client.user.tag} Successfully Has Gone ${client.gateway.presence.status.toUpperCase()}`);
     }
 }
