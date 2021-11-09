d0bcad1fb86b98f673a6fe9dcfe041942445d930
Reinhardt <notmarx.tech@gmail.com>
2021-11-09 16:23:07 +0700

    Delete ping.ts

--- src/commands/general/ping.ts
+++
@@ -1,9 +0,0 @@
-import { Command } from "../../interfaces";
-
-export const command: Command = {
-    name: "ping",
-    aliases: ["pong", "test"],
-    run: async (client, payload) => {
-        return client.replyMessage(payload, { content: `Gateway: ${(await client.ping()).gateway}ms | REST: ${(await client.ping()).rest}ms` })
-    }
-}
