5d28a3d1e2941d585e9466345a27744132a28be1
Reinhardt <notmarx.tech@gmail.com>
2021-11-09 15:59:51 +0700

    Update client.ts

--- src/client.ts
+++ src/client.ts
@@ -1,18 +1,47 @@
 import { ShardClient, ShardClientRunOptions } from "detritus-client";
 import { Database } from "./api";
+import { Command, Event } from "./interfaces";
 import * as Config from "../config.json";
 import { join } from "path";
 import { readdirSync } from "fs";
+import { Database as XenDatabase } from "xen.db";
 
 export default class BelfastClient extends ShardClient {
+    public commands = new Map<string, Command>();
     public config = Config;
-    public database = new Database();
+    public database = new XenDatabase("src/api/database/Belfast-Database.sql", { path: "src/api/database", table: "JSON", useWalMode: false });
+    public db = new Database();
+    public events = new Map<string, Event>();
 
-    public async launch(options?: ShardClientRunOptions): Promise<void> {
+    /**
+     * Initialize the bot to the Gateway 
+     * @param options Client Run Options
+     * @returns {Promise<void}
+     */
+     public async launch(options?: ShardClientRunOptions): Promise<void> {
         this.run(options);
+
+        const commandPath: string = join(__dirname, "commands");
+        
+        readdirSync(commandPath).forEach((dir) => {
+            const commands: string[] = readdirSync(`${commandPath}/${dir}`).filter((file) => file.endsWith(".ts"));
+
+            for (const file of commands) {
+                const { command } = require(`${commandPath}/${dir}/${file}`);
+                this.commands.set(command.name, command);
+            }
+        });
+
+        const eventPath: string = join(__dirname, "events");
+
+        readdirSync(eventPath).forEach(async (file) => {
+            const { event } = await import(`${eventPath}/${file}`);
+            this.events.set(event.name, event);
+            this.on(event.name, event.run.bind(null, this));
+        });
     }
 
     public launchDB(): void {
-        this.database.launch();
+        this.db.launch();
     }
 }
