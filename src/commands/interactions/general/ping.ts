56d5e1ae463108981db6343928df6b0bdbc333fa
Reinhardt <notmarx.tech@gmail.com>
2021-11-12 16:57:27 +0700

    Add ping interaction command

---
+++ src/commands/interactions/general/ping.ts
@@ -0,0 +1,12 @@
+import { InteractionCallbackTypes, MessageFlags } from "detritus-client/lib/constants";
+import { InteractionCommand } from "../../../interfaces";
+
+export const command: InteractionCommand = {
+    name: "ping",
+    run: async (client, payload) => {
+        payload.interaction.respond(InteractionCallbackTypes.CHANNEL_MESSAGE_WITH_SOURCE, {
+            content: `Gateway: ${(await client.ping()).gateway}ms | REST: ${(await client.ping()).rest}ms`,
+            flags: MessageFlags.EPHEMERAL
+        });
+    }
+}
