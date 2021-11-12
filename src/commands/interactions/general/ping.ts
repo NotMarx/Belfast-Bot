b3f748d5571403526f813a82d930f30794bb22de
Reinhardt <notmarx.tech@gmail.com>
2021-11-12 18:42:58 +0700

    Add meta desc

--- src/commands/interactions/general/ping.ts
+++ src/commands/interactions/general/ping.ts
@@ -3,6 +3,7 @@ import { InteractionCommand } from "../../../interfaces";
 
 export const command: InteractionCommand = {
     name: "ping",
+    description: "Ping the Bot",
     run: async (client, payload) => {
         payload.interaction.respond(InteractionCallbackTypes.CHANNEL_MESSAGE_WITH_SOURCE, {
             content: `Gateway: ${(await client.ping()).gateway}ms | REST: ${(await client.ping()).rest}ms`,
