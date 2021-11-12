12fedfe4501ee814822ac514b1f3344d984d9f7b
Reinhardt <notmarx.tech@gmail.com>
2021-11-12 19:31:59 +0700

    Add Erela filters plugin

---
+++ src/api/music/filter/filterconstants.ts
@@ -0,0 +1,23 @@
+export default class FilterConstants {
+    volume: number;
+    equalizer: any[] | null;
+    karaoke: unknown | null;
+    timescale: unknown | null;
+    tremolo: unknown | null;
+    vibrato: unknown | null;
+    rotation: unknown | null;
+    distortion: unknown | null;
+    lowpass: unknown | null;
+
+    constructor(filterOptions?: any) {
+        this.volume = 1.0;
+        this.equalizer = filterOptions?.equalizer ?? [];
+        this.karaoke = filterOptions?.karaoke ?? null;
+        this.timescale = filterOptions?.timescale ?? null;
+        this.tremolo = filterOptions?.tremolo ?? null;
+        this.vibrato = filterOptions?.vibrato ?? null;
+        this.rotation = filterOptions?.rotation ?? null;
+        this.distortion = filterOptions?.distortion ?? null;
+        this.lowpass = filterOptions?.lowpass ?? null;
+    }
+}
