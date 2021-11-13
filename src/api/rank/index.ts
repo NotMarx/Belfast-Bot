023625c21a5dd4ee6581079b12cee83dca95e618
Reinhardt <notmarx.tech@gmail.com>
2021-11-13 19:06:22 +0700

    Add comment line

--- src/api/rank/index.ts
+++ src/api/rank/index.ts
@@ -2,6 +2,13 @@ import BelfastClient from "../../client";
 import { Message } from "detritus-client/lib/structures"; 
 
 export default class LevelXP {
+
+    /**
+     * Get a level
+     * @param xp The XP
+     * @param extra Whether to enable extra mode
+     * @returns {Number | Array<Number>}
+     */
     static getLevel(xp: number, extra = false): number | number[] {
         let level = 0;
 
@@ -17,10 +24,20 @@ export default class LevelXP {
         }
     }
 
+    /**
+     * Get a level XP
+     * @param level The level
+     * @returns {Number}
+     */
     static getLevelXP(level: number): number {
         return 5 * Math.pow(level, 2) + 50 * level + 100;
     }
 
+    /**
+     * Get the rank info from the EXP given
+     * @param exp The EXP
+     * @returns {Object}
+     */
     static getInfo(exp: number) {
         let [level, remxp]: any = this.getLevel(exp, true);
         let levelXP = this.getLevelXP(level);
@@ -28,7 +45,13 @@ export default class LevelXP {
         return { level, remxp, levelXP };
     }
 
-    static addExp(client: BelfastClient, message: Message) {
+    /**
+     * Add an EXP to the user
+     * @param client Belfast client
+     * @param message The message payload
+     * @returns {void}
+     */
+    static addExp(client: BelfastClient, message: Message): void {
         let toAdd = Math.floor(Math.random() * 5 + 5);
         let oldXP: number = client.database.get(`Database.${message.guildId}.LevelXP.${message.author.id}`);
         let oldLevel = this.getLevel(oldXP);
@@ -37,6 +60,7 @@ export default class LevelXP {
 
         client.database.add(`Database.${message.guildId}.LevelXP.${message.author.id}`, toAdd);
 
+        // Sends a congrats message whenever user ranks up
         if (newLevel > oldLevel) {
             message.channel.createMessage({ content: `${message.author.mention}, You just reached Level **${newLevel}**!` });
         }
--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,10 +1,22 @@
 import byteSize from "byte-size";
 
 export default class Util {
-    static bytes(size: number): any {
+
+    /**
+     * Returns the current size of something
+     * @param size The number size
+     * @returns {Number}
+     */
+    static bytes(size: number): number {
         return byteSize(size);
     }
 
+    /**
+     * Round a number
+     * @param value The number to be rounded
+     * @param precision How many decimal numbers should be there
+     * @returns {Number}
+     */
     static round(value: number, precision: number): number {
         const multi = Math.pow(10, precision || 0);
         return Math.round(value * multi) / multi;
