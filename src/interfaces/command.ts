503dd4495c5bbc7e7797eacb61a1c095e2c093c4
Reinhardt <notmarx.tech@gmail.com>
2021-11-15 18:36:43 +0700

    Fix typescript

--- src/interfaces/command.ts
+++ src/interfaces/command.ts
@@ -2,7 +2,7 @@ import BelfastClient from "../client";
 import { GatewayClientEvents } from "detritus-client";
 
 interface CommandRun {
-    (client: BelfastClient, payload: GatewayClientEvents.MessageCreate, args?: Array<String>): Promise<any>;
+    (client: BelfastClient, payload: GatewayClientEvents.MessageCreate, args?: string[]): Promise<any>;
 }
 
 export interface Command {
