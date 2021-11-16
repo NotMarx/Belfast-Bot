87e141123d5152e350ed980b0a683074b8a35f43
Reinhardt <notmarx.tech@gmail.com>
2021-11-16 14:34:26 +0700

    Add nightcore effect

--- src/commands/prefixed/music/filter.ts
+++ src/commands/prefixed/music/filter.ts
@@ -9,5 +9,16 @@ export const command: Command = {
     run: async (client, payload, args) => {
         const effectFlag = await yargs(args.slice(0)).array(["effect"]).argv;
         const activeFlag = await yargs(args.slice(0)).boolean(["add", "remove"]).argv;
+        const player = client.manager.players.get(payload.message.guildId) as Player;
+
+        if (effectFlag.effect[0] === "nightcore") {
+            if (activeFlag.add) {
+                client.replyMessage(payload, { content: "Nightcore Activated!" });
+                player.setNightcore(true);
+            } else if (activeFlag.remove) {
+                client.replyMessage(payload, { content: "Nightcore Deactivated!" });
+                player.setNightcore(false);
+            }
+        }
     }
 }
